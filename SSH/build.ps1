b567e0dc427d117c07dd8a133b5c748fa4686b56
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-09-03 13:20:14 +0800

    ignore

--- SSH/build.ps1
+++ SSH/build.ps1
@@ -2,32 +2,31 @@ param(
 	[switch]$skipBuild
 )
 
-pushd
-cd $PSScriptRoot
+Push-Location $PSScriptRoot
 
 $version = Read-Host -Prompt 'New tag'
 
-if (-not $skipBuild){
-	dotnet build --configuration Release -p:GenerateAppxPackageOnBuild=true '-p:Platform=x64'
-	dotnet build --configuration Release -p:GenerateAppxPackageOnBuild=true '-p:Platform=arm64'
-	rm ./out/*
+if (-not $skipBuild) {
+	dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=x64
+	dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=arm64
+	Remove-Item ./out/*
 	(Get-ChildItem -r *.msix -Exclude Microsoft.WindowsAppRuntime.1.6.msix).FullName | ForEach-Object { Copy-Item $_ ./out/. -Force }
 }
 
-(cat ./app.manifest -Raw) -replace `
+(Get-Content ./app.manifest -Raw) -replace `
 	'<assemblyIdentity version="[\d\.]+" name="ProcessKiller.app"/>', `
 	"<assemblyIdentity version=`"$version.0`" name=`"ProcessKiller.app`"/>" `
-		| Out-File .\app.manifest -NoNewline
-(cat ./Package.appxmanifest -Raw) -replace `
+| Out-File .\app.manifest -NoNewline
+(Get-Content ./Package.appxmanifest -Raw) -replace `
 	'Publisher="CN=8LWXpg"
     Version="0.0.1.0" />', `
 	"Publisher=`"CN=8LWXpg`"
     Version=`"$version.0`" />" `
-		| Out-File ./Package.appxmanifest -NoNewline
+| Out-File ./Package.appxmanifest -NoNewline
 
 git add ..
 git commit -m 'bump'
 git tag "v$version"
 
-popd
+Pop-Location
 
