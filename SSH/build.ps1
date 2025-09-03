d50dab1d7bcd401f47d1ea3d74449d9253419da2
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-09-03 20:15:19 +0800

    bump

--- SSH/build.ps1
+++ SSH/build.ps1
@@ -1,27 +1,28 @@
 param(
-	[switch]$skipBuild
+    [switch]$skipBuild
 )
 
 Push-Location $PSScriptRoot
 
 $version = Read-Host -Prompt 'New tag'
 
-if (-not $skipBuild) {
-	dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=x64
-	dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=arm64
-	Remove-Item ./out/* -ea Ignore
-	mkdir ./out -ea ig
-	(Get-ChildItem -r *.msix -Exclude Microsoft.WindowsAppRuntime.*.msix).FullName | ForEach-Object { Copy-Item $_ ./out/. -Force }
+if (-not $skipBuild)
+{
+    dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=x64
+    dotnet build -c Release -p:GenerateAppxPackageOnBuild=true -p:Platform=arm64
+    Remove-Item ./out/* -ea Ignore
+    mkdir ./out -ea ig
+    (Get-ChildItem -r *.msix -Exclude Microsoft.WindowsAppRuntime.*.msix).FullName | ForEach-Object { Copy-Item $_ ./out/. -Force }
 }
 
-(Get-Content ./app.manifest -Raw) -replace `
-	'<assemblyIdentity version="[\d\.]+" name="ProcessKiller.app"/>', `
-	"<assemblyIdentity version=`"$version.0`" name=`"ProcessKiller.app`"/>" `
+(Get-Content ./app.manifest -Raw) -replace  `
+	 '<assemblyIdentity version="[\d\.]+" .*/>',  `
+	 "<assemblyIdentity version=`"$version.0`" .*/>" `
 | Out-File .\app.manifest -NoNewline
-(Get-Content ./Package.appxmanifest -Raw) -replace `
-	'Publisher="CN=8LWXpg"
-    Version="[\d\.]+" />', `
-	"Publisher=`"CN=8LWXpg`"
+(Get-Content ./Package.appxmanifest -Raw) -replace  `
+	 'Publisher="CN=8LWXpg"
+    Version="[\d\.]+" />',  `
+	 "Publisher=`"CN=8LWXpg`"
     Version=`"$version.0`" />" `
 | Out-File ./Package.appxmanifest -NoNewline
 
